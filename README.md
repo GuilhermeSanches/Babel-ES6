0eff6cf01c1fe62c06683d67b0936266f97af8f6
Guilherme Sanches Pereira <guilherme.sanches@inatel.br>
2016-06-16 13:37:47 -0300

    alter index

---
+++ README.md
@@ -0,0 +1,14 @@
+# Example Basic with Babel
+Project using babel and ES6
+
+## How to run
+After the downloading or cloning the repository, use:
+```bash
+npm install
+npm build run
+```
+
+## Copyright and License
+Copyright 2016 Guilherme Sanches
+
+This software is released under the MIT license.
--- app/lib/main.js
+++ app/lib/main.js
@@ -1,44 +1,42 @@
-class Init{
-    
-    constructor(){
-       
-
-
+class Init  {
+   
+    constructor() {
+            
         this.label = document.getElementById('label');
         this.users = [];
         this.element = document.getElementsByClassName('users')[0];
-                        
+
     }
 
-    populateList(){
-        
+    populateList() {
+
         this.users = [
-        {
-            name: 'Guilherme',
-            age: 22
-        },
-        {
-            name: 'Auro',
-            age: 22
-        },
-        {
-            name: 'Teste',
-            age: 22
-        }
+            {
+                name: 'Guilherme',
+                age: 22
+            },
+            {
+                name: 'Auro',
+                age: 22
+            },
+            {
+                name: 'Teste',
+                age: 22
+            }
         ]
     }
-    
-    updateView(){
-             
-       let that = this;      
-       for(let user of that.users ){
-           
-               var elementChild = that.element.cloneNode(true);
-               elementChild.innerHTML = user.name;
-               that.element.appendChild(elementChild);
-               
-        }              
-    }   
+
+    updateView() {
+
+        let that = this;
+        for (let user of that.users) {
+
+            var elementChild = that.element.cloneNode(true);
+            elementChild.innerHTML = user.name;
+            that.element.appendChild(elementChild);
+
+        }
+    }
 }
 
 let init = new Init();
